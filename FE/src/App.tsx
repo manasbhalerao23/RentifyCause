cfb7b046f3d38b91800535263c8a423ef451def0
Manas Bhalerao <manasbhalerao1@gmail.com>
2025-02-19 01:13:32 +0530

    header added

---
+++ FE/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Header from './components/Header';
+function App() {
+ 
+  return (
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Header/>}/>
+    </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default App
---
+++ FE/src/components/Header.tsx
@@ -0,0 +1,44 @@
+import {CircleUserRound, Facebook, Twitter, Youtube} from 'lucide-react';
+
+function Header() {
+    return(
+        <header>
+            <div className='flex justify-between items-center px-6 py-2 text-sm bg-red-600'>
+                <div className='flex space-x-4 '>
+                    <button className='cursor-pointer'>
+                        Hindi
+                    </button>
+                </div>
+                <div className='cursor-pointer flex space-x-3'>
+                    <Twitter/>
+                    <Facebook/>
+                    <Youtube/>
+                </div>
+            </div>
+            {/*main part*/}
+            <div className=' flex justify-between items-center px-6 py-6 bg-red-100 text-black border-b-2 border-red-200'>
+                <div className='flex items-center space-x-4'>
+                    <div>
+                        <h1 className='text-xl font-bold'>
+                            Red Cross
+                        </h1>
+                        <p className='text-sm'>
+                            indore,mp.
+                        </p>
+                    </div>
+                </div>
+                <div className='flex space-x-3 items-center gap-7'>
+                    <button className='bg-red-600 text-white px-4 py-2 rounded-3xl cursor-pointer hover:bg-teal-500 transition duration-500 animate-pulse ease-in-out '>
+                        Rent
+                    </button>
+                    <button className='rounded-md flex gap-1 font-medium cursor-pointer'>
+                        <CircleUserRound/>
+                        Profile
+                    </button>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
